801e4ed68e7260e425dfd874e5c937cd44667457
엄태권 <akfls221@gmail.com>
2023-02-20 01:26:50 +0900

    feat : 테스트 코드들 작성

--- src/main/java/org/data_structures/list/Main.java
+++ src/main/java/org/data_structures/list/Main.java
@@ -1,13 +1,30 @@
 package org.data_structures.list;
 
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        /**
+         * MyList 생성 테스트
+         */
         MyList<Integer> myList = MyList.testOf(1, 2);
+
+        /**
+         * MyList add(삽입) 테스트
+         */
         myList.add(3, 2);
 
-        System.out.println("myList = " + myList);
+        /**
+         * MyList delete(삭제) 테스트
+         */
+        myList.delete(1);
+
+        /**
+         * MyList get(조회) 테스트
+         */
+        myList.get(2);
 
     }
 }
