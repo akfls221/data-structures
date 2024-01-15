6c12377ed9cf0024d628eca913f8cc6f60fe8770
엄태권 <akfls221@gmail.com>
2024-01-16 01:54:18 +0900

    feat : push top 메소드 추가

--- src/main/java/org/data_structures/stackandqueue/liststack/ListStack.java
+++ src/main/java/org/data_structures/stackandqueue/liststack/ListStack.java
@@ -32,4 +32,32 @@ public class ListStack<E> {
 
         return this.top.getItem();
     }
+
+    /**
+     * 새 항목을 push 하는 메소드
+     * 
+     * @param newItem   추가 하려는 항목
+     */
+    public void push(E newItem) {
+        Node newNode = new Node(newItem, this.top); //리스트 앞부분에 삽입
+        this.top = newNode; //top이 새로 추가돈 노드를 가리킴
+        this.size++;    // 스택 항목 증가
+    }
+
+    /**
+     * 현재 top을 제거하는 메소드
+     * 
+     * @return E topItem 삭제된 아이템
+     */
+    public E pop() {
+        if (this.isEmpty()) {
+            throw new EmptyStackException();
+        }
+
+        E topItem = this.top.getItem(); // 현재 top 항목을 저장
+        this.top = this.top.getNext();    //top이 바로 아래 항목을 가리킴.
+        this.size--;    // 스택 항목수 감소
+
+        return topItem;
+    }
 }
