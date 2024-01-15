baf264c81ac20ca61785c9ff53420d4f677e356c
엄태권 <akfls221@gmail.com>
2024-01-16 01:06:06 +0900

    feat : 테스트 메소드 작성

--- src/main/java/org/data_structures/stackandqueue/Main.java
+++ src/main/java/org/data_structures/stackandqueue/Main.java
@@ -3,6 +3,16 @@ package org.data_structures.stackandqueue;
 public class Main {
     public static void main(String[] args) {
         ArrayStack<String> stack = new ArrayStack<>();
-        stack.peek();
+
+        stack.push("test1");
+        stack.push("test2");
+        System.out.println("stack = " + stack);
+
+        String peek = stack.peek();
+        System.out.println("peek = " + peek);
+
+        String pop = stack.pop();
+        System.out.println("pop = " + pop);
+        System.out.println("stack = " + stack);
     }
 }
