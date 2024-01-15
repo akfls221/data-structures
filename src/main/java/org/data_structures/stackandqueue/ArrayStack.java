50e638a1fc937f0bd59c50ee66554cdf4a2c999e
엄태권 <akfls221@gmail.com>
2024-01-16 00:44:04 +0900

    feat : 스택에 값 추가 및 resize 메소드 추가

--- src/main/java/org/data_structures/stackandqueue/ArrayStack.java
+++ src/main/java/org/data_structures/stackandqueue/ArrayStack.java
@@ -26,6 +26,33 @@ public class ArrayStack<E> {
 
         return this.s[this.top];    //배열의 가장 마지막(stack에선 가장위) 항목 반환
     }
+
+    /**
+     * 새 항복을 스택에 삽입한다.
+     *
+     * @param newItem   추가할 Item
+     */
+    public void push(E newItem) {
+        //이미 배열이 꽉 찬 상태라면
+        if (this.size() == this.s.length) {
+            reSize(2*this.s.length);
+        }
+
+        this.s[this.top++] = newItem;
+    }
+
+    /**
+     * 배열이 꽉찼을 경우 크기를 2배로 늘린다.
+     * 
+     * @param reSize    새롭게 늘릴 배열 크기
+     */
+    private void reSize(int reSize) {
+        Object[] t = new Object[reSize];
+        for (int i = 0; i < this.size(); i++) {
+            t[i] = this.s[i];
+        }
+        this.s = (E[]) t;
+    }
     
     
 }
