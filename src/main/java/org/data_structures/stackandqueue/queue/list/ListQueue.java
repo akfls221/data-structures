d2f1f1595c02bd47f34c2ddcc09e3ed5f20c62d2
엄태권 <akfls221@gmail.com>
2024-01-17 03:27:15 +0900

    feat : add 메소드 생성

--- src/main/java/org/data_structures/stackandqueue/queue/list/ListQueue.java
+++ src/main/java/org/data_structures/stackandqueue/queue/list/ListQueue.java
@@ -13,6 +13,23 @@ public class ListQueue <E> {
         this.size = 0;
     }
 
+    /**
+     * 단순 연결 리스트 Queue 삽입
+     * 
+     * @param newItem 큐에 삽입할 아이템
+     */
+    public void add(E newItem) {
+        Node<E> addItem = new Node<>(newItem, null);
+        if (isEmpty()) {
+            this.front = addItem;
+        } else {
+            this.rear.setNext(addItem);
+        }
+
+        this.rear = addItem;
+        this.size++;
+    }
+
     public int size() {
         return this.size;
     }
