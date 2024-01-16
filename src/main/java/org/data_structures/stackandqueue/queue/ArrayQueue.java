c120efae42a4cda163872c4fdfe41e5dcdaf9368
엄태권 <akfls221@gmail.com>
2024-01-17 02:14:07 +0900

    feat : queue resize 메소드 및 add 메소드 추가

--- src/main/java/org/data_structures/stackandqueue/queue/ArrayQueue.java
+++ src/main/java/org/data_structures/stackandqueue/queue/ArrayQueue.java
@@ -12,11 +12,36 @@ public class ArrayQueue <E> {
         this.front = this.rear = this.size = 0;
     }
 
+    /**
+     * Queue 삽입 연산
+     * 
+     * @param newItem   큐에 추가할 새로운 아이템
+     */
+    public void add(E newItem) {
+        if ((this.rear + 1) % this.queue.length == this.front) { //빈 원소가 1개뿐인 경우 resize(큐가 full인 경우)
+            resize(2 * this.queue.length);
+        }
+
+        this.rear = (this.rear + 1) % this.queue.length;
+        this.queue[this.rear] = newItem;
+        this.size++;
+    }
+
     public int size() {
         return this.size;
     }
 
     public boolean isEmpty() {
         return this.size == 0;
+    }
+
+    private void resize(int resize) {
+        Object[] resizeQueue = new Object[resize];
+
+        for (int i = 0; i < this.queue.length; i++) {
+            resizeQueue[i] = this.queue[i];
+        }
+
+        this.queue = (E[]) resizeQueue;
     }
 }
