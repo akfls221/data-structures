fb6854c24eface8952e5371e3c29d59fecf38411
엄태권 <akfls221@gmail.com>
2024-01-19 01:55:00 +0900

    feat : 레벨순회 테스트 추가

--- src/main/java/org/data_structures/treestructures/binarytree/Main.java
+++ src/main/java/org/data_structures/treestructures/binarytree/Main.java
@@ -7,11 +7,17 @@ public class Main {
         BinaryTree<String> keyBinaryTree = new BinaryTree<>();
         keyBinaryTree.setRoot(rootNode);
 
+        System.out.println("중위순회");
         keyBinaryTree.inorder(rootNode);
+        System.out.println("전위순회");
         keyBinaryTree.preorder(rootNode);
 
 
         Node<String> rootNode2 = new Node<>("A", new Node<>("B", new Node<>("D", new Node<>("G", null, null), new Node<>("H", null, null)), null), null);
+        System.out.println("후위순회");
         keyBinaryTree.postOrder(rootNode2);
+
+        System.out.println("레벨순회");
+        keyBinaryTree.levelorder(rootNode2);
     }
 }
