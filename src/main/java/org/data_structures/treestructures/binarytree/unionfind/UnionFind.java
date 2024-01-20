fc993c73211cd10bd21c0dda368cd44f21781582
엄태권 <akfls221@gmail.com>
2024-01-21 01:18:11 +0900

    feat : rank 기반의 union 연산 메소드 추가

--- src/main/java/org/data_structures/treestructures/binarytree/unionfind/UnionFind.java
+++ src/main/java/org/data_structures/treestructures/binarytree/unionfind/UnionFind.java
@@ -18,4 +18,31 @@ public class UnionFind {
 
         return a[i].getParent();
     }
+
+    /**
+     * rank 기반의 union 연산
+     *
+     * @param i 비교하려는 i root
+     * @param j 비교하려는 j root
+     */
+    public void union(int i, int j) {
+        int iroot = find(i);
+        int jroot = find(j);
+
+        if (iroot == jroot) {
+            return;
+        }
+
+        // rank가 높은 루트가 승자가 된다.
+        if (a[iroot].getRank() > a[jroot].getRank()) {
+            a[jroot].setParent(iroot);
+        } else if (a[iroot].getRank() < a[jroot].getRank()) {
+            a[iroot].setParent(jroot);
+        } else {
+            //둘 중의 하나를 임의로 승자로 취급 -> rank 1증가
+            a[jroot].setParent(iroot);
+            int t = a[iroot].getRank() + 1;
+            a[iroot].setRank(t);
+        }
+    }
 }
