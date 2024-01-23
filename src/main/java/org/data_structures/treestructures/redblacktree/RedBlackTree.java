c2ad41332c4260d1928807ae5050a3b9230b1fc5
엄태권 <akfls221@gmail.com>
2024-01-24 00:37:57 +0900

    feat :deleteMin 메소드 추가

--- src/main/java/org/data_structures/treestructures/redblacktree/RedBlackTree.java
+++ src/main/java/org/data_structures/treestructures/redblacktree/RedBlackTree.java
@@ -94,6 +94,18 @@ public class RedBlackTree<Key extends Comparable<Key>, Value> {
         return node;
     }
 
+    private Node deleteMin(Node node) {
+        if (node.left == null) {
+            return null;
+        }
+
+        if (!isRed(node.left) && !isRed(node.left.left)) {
+            node = moveRedLeft(node);
+        }
+        node.left = deleteMin(node.left);
+        return fixUp(node);
+    }
+
     /**
      * 좌편향 레드블랙 트리의 규칙을 위반하지 않는 상태를 유지하기 위해 아래 두 가지 방법으로 레드 link를 왼쪽 아래로 내려 보낸다.
      * case 1 : n.left 와 n.left.left가 모두 블랙이고, 동시에 n.right.left도 블랙이면 flipColors(node)를 실행한다.
