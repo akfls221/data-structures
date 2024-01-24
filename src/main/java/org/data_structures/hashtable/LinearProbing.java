6d5f1913082f2ca08d27b2ef224029b58dff39fd
엄태권 <akfls221@gmail.com>
2024-01-25 00:17:06 +0900

    feat : key와 value를 저장하는 put 메소드 추가

--- src/main/java/org/data_structures/hashtable/LinearProbing.java
+++ src/main/java/org/data_structures/hashtable/LinearProbing.java
@@ -15,4 +15,32 @@ public class LinearProbing<K, V> {
     private int hash(K key) { //해시 함수
         return (key.hashCode() & 0x7fffffff) % M; //나눗셉 함수
     }
+
+    /**
+     * key 와 value 저장
+     * 
+     * @param key   삽입 하려는 key
+     * @param value 삽입 하려는 value
+     */
+    private void put(K key, V value) { //삽입 연산
+        int initialpos = hash(key); // 초기 시작위치
+        int i = initialpos;
+        int j = 1;
+
+        do {
+            if (a[i] == null) { // 삽입 위치 발견
+                a[i] = key; // key를 해시 테이블에 저장
+                d[i] = value; // value를 동일한 인덱스 하에 저장
+                return;
+            }
+
+            if (a[i].equals(key)) { //이미 존재하는 key
+                d[i] = value;       // value만 갱신
+                return;
+            }
+
+            i = (initialpos + j++) % M; // i = 다음 위치
+        } while (i != initialpos);
+        System.out.println("저장 실패");
+    }
 }
