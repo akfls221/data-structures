2fe3a0853926d37a528e828a1e4534517ba992d0
엄태권 <akfls221@gmail.com>
2024-01-29 01:33:02 +0900

    feat : Huffman class 추가

---
+++ src/main/java/org/data_structures/priorityqueue/huffman/Huffman.java
@@ -0,0 +1,12 @@
+package org.data_structures.priorityqueue.huffman;
+
+public class Huffman {
+
+    private Entry[] a; // a[0]은 미사용
+    private int N;//힙의 크기
+
+    public Huffman(Entry[] a, int initialSize) {
+        this.a = a;
+        N = initialSize;
+    }
+}
