37622bd02f07ca39a6a2f2f0066435779caef8b0
엄태권 <akfls221@gmail.com>
2024-01-28 18:05:32 +0900

    feat : 자식 노드와 비교하는 downHeap 메소드 추가

--- src/main/java/org/data_structures/priorityqueue/binaryheap/BHeap.java
+++ src/main/java/org/data_structures/priorityqueue/binaryheap/BHeap.java
@@ -9,6 +9,25 @@ public class BHeap<Key extends Comparable<Key>, Value> {
         N = n;
     }
 
+    /**
+     * 자식 노드와 비교하는 downHeap 메소드
+     * @param i
+     */
+    private void downheap(int i) {
+        while (2 * i <= N) {    // 왼쪽 자식이 힙에 있으면
+            int k = 2 * i;      // k = 왼쪽 자식의 인덱스
+            if (k < N && greater(k, k + 1)) {   //k 가 승자
+                k++;    //의미 없는 코드인듯 후위연산자를 사용하는 의미없는 코드
+            }
+
+            if (!greater(i, k)) {   //현재 노드가 자식의 승자와 같거나 작으면 루프 중단
+                break;
+            }
+            swap(i, k);
+            i = k;
+        }
+    }
+
     public int size() {
         return N;
     }
