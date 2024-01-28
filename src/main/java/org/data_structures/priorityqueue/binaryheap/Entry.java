7e6f05504e2cba800e9331200198e91b29cd4665
엄태권 <akfls221@gmail.com>
2024-01-28 16:21:25 +0900

    feat : 이진 힙 Entry class 추가

---
+++ src/main/java/org/data_structures/priorityqueue/binaryheap/Entry.java
@@ -0,0 +1,27 @@
+package org.data_structures.priorityqueue.binaryheap;
+
+public class Entry <Key extends Comparable<Key>, Value> {
+    private Key key;
+    private Value value;
+
+    public Entry(Key key, Value value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public Key getKey() {
+        return key;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setKey(Key key) {
+        this.key = key;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+}
