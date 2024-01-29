089da703f295782f6668411c1feb63f7186d3a3b
엄태권 <akfls221@gmail.com>
2024-01-30 01:58:00 +0900

    feat : isless, swap 메소드 추가

---
+++ src/main/java/org/data_structures/sort/insertion/Insertion.java
@@ -0,0 +1,27 @@
+package org.data_structures.sort.insertion;
+
+public class Insertion {
+    /**
+     *  키 비교 메소드
+     *
+     * @param i
+     * @param j
+     * @return
+     */
+    private static boolean isless(Comparable i, Comparable j) {
+        return (i.compareTo(j) < 0);
+    }
+
+    /**
+     * 원소 교환 메소드 최솟값을 a[i]와 바꾼다
+     *
+     * @param a     전체 배열
+     * @param i     정렬된 배열 오른쪽 인덱스
+     * @param j     정렬된 배열 오른쪽 기준 최솟값
+     */
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable temp = a[i]; //정렬된 배열의 바로 오른쪽 인덱스 값
+        a[i] = a[j];    // 정렬된 배열 오른쪽 기준 최솟값으로 교체
+        a[j] = temp;    // 해당 부분에 temp로 교체
+    }
+}
