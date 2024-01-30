9d5da1c9a6bab9c098d36c2ef14568917fe5423a
엄태권 <akfls221@gmail.com>
2024-01-31 03:30:33 +0900

    feat : swap 메소드 및 Quick 정랼 추가

---
+++ src/main/java/org/data_structures/sort/quick/Quick.java
@@ -0,0 +1,10 @@
+package org.data_structures.sort.quick;
+
+public class Quick {
+
+    private static void swap(Comparable[] a, int i, int j) {
+        Comparable temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
