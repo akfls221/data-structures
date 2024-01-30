0b101eb739e675f262493425e4b70b6cfdbae2f2
엄태권 <akfls221@gmail.com>
2024-01-31 03:14:01 +0900

    feat : sort메소드 추가

--- src/main/java/org/data_structures/sort/merge/Merge.java
+++ src/main/java/org/data_structures/sort/merge/Merge.java
@@ -2,6 +2,22 @@ package org.data_structures.sort.merge;
 
 public class Merge {
 
+    public static void sort(Comparable[] a) {
+        Comparable[] b = new Comparable[a.length];
+        sort(a, b, 0, a.length -1);
+    }
+
+    private static void sort(Comparable[] a, Comparable[] b, int low, int high) {
+        if (high <= low) {
+            return;
+        }
+
+        int mid = low + (high - low) / 2;   //중간 index
+        sort(a, b, low, mid);   //앞부분 순환 호출
+        sort(a, b, mid + 1, high);  //뒷부분 순환 호출
+        merge(a, b, 0, a.length - 1);
+    }
+
     private static boolean isLess(Comparable v, Comparable w) {
         return (v.compareTo(w) < 0);
     }
