d2a67463de7dd6c287f7351cd3d0baa5fb6772f6
엄태권 <akfls221@gmail.com>
2024-01-31 03:40:52 +0900

    feat : partition 메소드 추가

--- src/main/java/org/data_structures/sort/quick/Quick.java
+++ src/main/java/org/data_structures/sort/quick/Quick.java
@@ -2,6 +2,31 @@ package org.data_structures.sort.quick;
 
 public class Quick {
 
+    private static int partition(Comparable[] a, int pivot, int high) {
+        int i = pivot;
+        int j = high + 1;
+        Comparable p = a[pivot];
+
+        while (true) {
+            while (isLess(a[++i], p)) { //피벗 보다 작으면
+                if (i == high) {
+                    break;
+                }
+                while (isLess(p, a[--j])) { // 피벗보다 크면
+                    if (j == pivot) {
+                        break;
+                    }
+                }
+
+                if (i >= j) {
+                    break;
+                }
+                swap(a, i, j);
+                return j;
+            }
+        }
+    }
+
 
     private static boolean isLess(Comparable v, Comparable w) {
         return (v.compareTo(w) < 0);
