058c8d629abf0c7b3992211c6516bd2c88e005e6
엄태권 <akfls221@gmail.com>
2024-01-31 03:30:58 +0900

    feat : isLess 메소드 추가

--- src/main/java/org/data_structures/sort/quick/Quick.java
+++ src/main/java/org/data_structures/sort/quick/Quick.java
@@ -2,6 +2,11 @@ package org.data_structures.sort.quick;
 
 public class Quick {
 
+
+    private static boolean isLess(Comparable v, Comparable w) {
+        return (v.compareTo(w) < 0);
+    }
+
     private static void swap(Comparable[] a, int i, int j) {
         Comparable temp = a[i];
         a[i] = a[j];
