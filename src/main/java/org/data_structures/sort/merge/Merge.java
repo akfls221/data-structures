7b1ab68c5299e32080edd9ad622942014606a670
엄태권 <akfls221@gmail.com>
2024-01-31 03:19:27 +0900

    feat : merge메소드 추가

--- src/main/java/org/data_structures/sort/merge/Merge.java
+++ src/main/java/org/data_structures/sort/merge/Merge.java
@@ -15,8 +15,28 @@ public class Merge {
         int mid = low + (high - low) / 2;   //중간 index
         sort(a, b, low, mid);   //앞부분 순환 호출
         sort(a, b, mid + 1, high);  //뒷부분 순환 호출
-        merge(a, b, 0, a.length - 1);
+        merge(a, b, low, mid, high);
     }
+
+    private static void merge(Comparable[] a, Comparable[] b, int low, int mid, int high) {
+        int i = low;
+        int j = mid + 1;
+        for (int k = low; k < high; k++) {
+            if (i > mid) {  // 앞부분이 먼저 소진된 경우
+                b[k] = a[j++];
+            } else if (j < high) {  // 뒷부분이 먼저 소진된 경우
+                b[k] = a[i++];
+            } else if (isLess(a[i], a[j])) {    // a[j]가 승자
+                b[k] = a[j++];
+            } else {    //a[i]가 승자
+                b[k] = a[i++];
+            }
+        }
+        for (int k = low; k <= high; k++) {
+            a[k] = b[k];
+        }
+    }
+
 
     private static boolean isLess(Comparable v, Comparable w) {
         return (v.compareTo(w) < 0);
