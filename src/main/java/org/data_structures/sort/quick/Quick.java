36d1de0482991a53833e18aea9296c04d8fea765
엄태권 <akfls221@gmail.com>
2024-01-31 03:44:46 +0900

    feat : sort 메소드 추가

--- src/main/java/org/data_structures/sort/quick/Quick.java
+++ src/main/java/org/data_structures/sort/quick/Quick.java
@@ -1,6 +1,29 @@
 package org.data_structures.sort.quick;
 
+/**
+ * 피벗 정렬
+ *
+ * 최선 : O(n logn)
+ * 평균 : O(n logn)
+ * 최악 : O(n²)
+ */
 public class Quick {
+
+
+    public static void sort(Comparable[] a) {
+        sort(a, 0, a.length - 1);
+    }
+
+    private static void sort(Comparable[] a, int low, int high) {
+        if (high <= low) {
+            return;
+        }
+
+        int j = partition(a, low, high);
+        sort(a, low, j - 1);    // 피벗보다 작은 부분을 순환 호출
+        sort(a, j + 1, high);   // 피벗보다 큰 부분을 순환 호출
+        
+    }
 
     private static int partition(Comparable[] a, int pivot, int high) {
         int i = pivot;
